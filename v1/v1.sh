e2f0716021d434eae29d32c1ecc4a2e5704c6923
dbarelop <dbarelop@gmail.com>
2016-04-02 11:28:38 +0200

    Modified exercise 1 script (still getting kernel panic when executing Busybox)

--- v1/v1.sh
+++ v1/v1.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
 function create_initramfs {
-	mkdir initramfs
-	mv "$1" initramfs
-	cd initramfs
-	mkdir -p dev sbin bin usr/bin etc var tmp
-	mv "$1" bin/busybox
-	ln -s /bin/busybox bin/sh
+    mkdir initramfs
+    cd initramfs
+    mkdir -p dev sbin bin usr/bin etc var tmp
+    cd bin
+    cp "$1" busybox
+    chmod +x busybox
+    ln -s busybox sh
+    cd ..
+    
+    if [ -z "$2" ]; then
+        # Simple init file to launch busybox
+        echo "* No init file provided. Configuring initramfs to launch Busybox shell."
+        echo '#!/bin/sh' > init
+        echo 'exec /bin/sh' >> init
+    else
+        echo "* Using provided init file '$2'"
+        cp "$2" init
+    fi
+    chmod +x init
 
-	# Simple init file to launch busybox
-	cat > init << EOF
-#!/bin/sh
-#mount -t proc none /proc
-#mount -t sysfs none /sys
-exec /bin/sh
-EOF
-
-	chmod +x init
-	find . | cpio -H newc -o > ../initramfs.cpio
-	cd ..
-	rm -rf initramfs
+    find . | cpio -H newc -o > ../initramfs.cpio
+    cd ..
+    rm -rf initramfs
 }
 
-if [ $# -ne 2 ]; then
-	echo "Usage: $0 <kernel_config> <busybox_binary>"
-	exit 0
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <kernel_config> <busybox_binary> [init_file]"
+    exit 0
 fi
 
 OUTPUT="stdout_1.log"
 OUTPUT_ERR="errorout_1.log"
 VERSION="4.2.3"
-KERNEL_CONFIG="$1"
-BUSYBOX="$2"
+KERNEL_CONFIG="$(readlink -f $1)"
+BUSYBOX="$(readlink -f $2)"
+INIT="$(readlink -f $3 2> /dev/null)"
 CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
 export ARCH="x86"
 export CC="ccache gcc"
@@ -40,24 +45,23 @@ export CC="ccache gcc"
 #exec > $OUTPUT 2> $OUTPUT_ERR
 
 if [ ! -d "linux-$VERSION" ]; then
-	# Download the kernel if necessary
-	test -f "linux-$VERSION.tar.xz" || wget "https://kernel.org/pub/linux/kernel/v4.x/linux-$VERSION.tar.xz"
-	#test -f "linux-$VERSION.tar.sign" || wget "https://kernel.org/pub/linux/kernel/v4.x/linux-$VERSION.tar.sign"
-	#unxz "linux-$VERSION.tar.xz"
-	#gpg --verify "linux-$VERSION.tar.sign" || \
-	#	echo "Bad signature. Aborting." && \
-	#	rm -rf "linux-$VERSION.tar" && \
-	#	exit 1
-	test -d "linux-$VERSION" && rm -rf "linux-$VERSION"
-	xz -cd "linux-$VERSION.tar.xz" | tar xvf "linux-$VERSION.tar" -
-	#rm "linux-$VERSION.tar"
+    # Download the kernel if necessary
+    test -f "linux-$VERSION.tar.xz" || wget "https://kernel.org/pub/linux/kernel/v4.x/linux-$VERSION.tar.xz"
+    #test -f "linux-$VERSION.tar.sign" || wget "https://kernel.org/pub/linux/kernel/v4.x/linux-$VERSION.tar.sign"
+    #unxz "linux-$VERSION.tar.xz"
+    #gpg --verify "linux-$VERSION.tar.sign" || \
+    #   echo "Bad signature. Aborting." && \
+    #   rm -rf "linux-$VERSION.tar" && \
+    #   exit 1
+    test -d "linux-$VERSION" && rm -rf "linux-$VERSION"
+    xz -cd "linux-$VERSION.tar.xz" | tar xvf "linux-$VERSION.tar" -
+    #rm "linux-$VERSION.tar"
 fi
 cp "$KERNEL_CONFIG" "linux-$VERSION"/.config
-cp "$BUSYBOX" "linux-$VERSION"/busybox
 cd "linux-$VERSION"
 
 # Create initramfs file
-create_initramfs busybox
+create_initramfs "$BUSYBOX" "$INIT"
 
 # Compile
 make -j $CORES
