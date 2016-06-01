c08d914bd10c79c7a52f2e75545574cae707de66
dbarelop <dbarelop@gmail.com>
2016-06-02 00:10:03 +0200

    Modified template test script

--- v4/files/drivers/template/test_template.sh
+++ v4/files/drivers/template/test_template.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-make
-modinfo template.ko
-sudo insmod template.ko
-dmesg | grep -i template | tail -1
+MOD_NAME="template"
+
+echo
+echo "** Module info"
+modinfo $MOD_NAME.ko
+
+echo
+echo "** Loading module..."
+sudo insmod $MOD_NAME.ko
+dmesg | grep -i $MOD_NAME | tail -1
+
+echo
+echo "** /proc/devices"
 cat /proc/devices
-sudo rmmod template
-dmesg | grep -i template | tail -1
+
+echo
+echo "** Removing module..."
+sudo rmmod $MOD_NAME
+dmesg | grep -i $MOD_NAME | tail -1
