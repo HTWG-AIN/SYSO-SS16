e492525d3b2b253741e52b5dee5ceeb83e8dfd80
dbarelop <dbarelop@gmail.com>
2016-06-16 14:05:29 +0200

    Added open_once module

---
+++ v5/module-src/open_once/open_once.c
@@ -0,0 +1,119 @@
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/sched.h>
+
+//#define CLASSIC_METHOD
+
+#define DEV_NAME "open_once"
+#ifndef CLASSIC_METHOD
+#define REGION_NAME "OPEN_ONCE"
+#define CLASS_NAME "open_once"
+#endif
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Daniel Barea López <da431lop@htwg-konstanz.de>");
+MODULE_AUTHOR("Nicolas Wehrle <niwehrle@htwg-konstanz.de>");
+MODULE_DESCRIPTION("Linux kernel module developed for the v4 exercise of Systemsoftware in the HTWG Konstanz");
+MODULE_DESCRIPTION("Critical section management with a mutex");
+MODULE_VERSION("0.1");
+
+#ifdef CLASSIC_METHOD
+static int major;
+#else
+static dev_t dev_number;
+static struct cdev *driver_object;
+static struct class *class;
+#endif
+
+static DEFINE_MUTEX(mutex);
+
+static int driver_open(struct inode *device_file, struct file *instance);
+static int driver_release(struct inode *device_file, struct file *instance);
+static ssize_t driver_read(struct file *instance, char __user *user, size_t count, loff_t *offset);
+
+static struct file_operations fops = {
+    .open = driver_open,
+    .release = driver_release,
+    .read = driver_read
+};
+
+static int __init mod_init(void) {
+    #ifdef CLASSIC_METHOD
+    if ((major = register_chrdev(0, DEV_NAME, &fops)) < 0) {
+        printk(KERN_ALERT DEV_NAME ": error registering device (%d)\n", major);
+        return major;
+    }
+    printk(KERN_INFO DEV_NAME ": device succesfully registered with major number %d\n", major);
+    #else
+    if (alloc_chrdev_region(&dev_number, 0, 1, REGION_NAME)) {
+        printk(KERN_ERR DEV_NAME ": error in alloc_chrdev_region\n");
+        return -EIO;
+    }
+    if ((driver_object = cdev_alloc()) == NULL) {
+        printk(KERN_ERR DEV_NAME ": error in cdev_alloc\n");
+        kobject_put(&driver_object->kobj);
+        unregister_chrdev_region(dev_number, 1);
+        return -EIO;
+    }
+    driver_object->owner = THIS_MODULE;
+    driver_object->ops = &fops;
+    if (cdev_add(driver_object, dev_number, 1)) {
+        printk(KERN_ERR DEV_NAME ": error in cdev_add\n");
+        kobject_put(&driver_object->kobj);
+        unregister_chrdev_region(dev_number, 1);
+        return -EIO;
+    }
+    class = class_create(THIS_MODULE, CLASS_NAME);
+    device_create(class, NULL, dev_number, NULL, "%s", DEV_NAME);
+    printk(KERN_INFO DEV_NAME ": device init succesfully completed\n");
+    #endif
+    return 0;
+}
+
+static void __exit mod_exit(void) {
+    #ifdef CLASSIC_METHOD
+    unregister_chrdev(major, DEV_NAME);
+    #else
+    device_destroy(class, dev_number);
+    class_destroy(class);
+    cdev_del(driver_object);
+    unregister_chrdev_region(dev_number, 1);
+    #endif
+    printk(KERN_INFO DEV_NAME ": device succesfully unregistered\n");
+}
+
+static int driver_open(struct inode *device_file, struct file *instance) {
+    printk(KERN_DEBUG DEV_NAME ": open called\n");
+    while (!mutex_trylock(&mutex)) {
+        printk(KERN_DEBUG DEV_NAME ": driver busy, retrying after 200 ms...");
+        // Sleep for 200 ms
+        schedule_timeout_interruptible(200 * HZ / 1000);
+        if (signal_pending(current)) {
+            printk(KERN_ERR DEV_NAME ": signal received\n");
+            mutex_unlock(&mutex);
+            return -EIO;
+        }
+    }
+    printk(KERN_DEBUG DEV_NAME ": mutex locked\n");
+    schedule_timeout_interruptible(3 * HZ);
+    printk(KERN_DEBUG DEV_NAME ": unlocking mutex\n");
+    mutex_unlock(&mutex);
+    return 0;
+}
+
+static int driver_release(struct inode *device_file, struct file *instance) {
+    printk(KERN_DEBUG DEV_NAME ": release called\n");
+    return 0;
+}
+
+static ssize_t driver_read(struct file *instance, char __user *user, size_t count, loff_t *offset) {
+    printk(KERN_INFO DEV_NAME ": read called\n");
+    return 0;
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
